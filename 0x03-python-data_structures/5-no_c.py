885087f98442fc6c4cc3e48724ffdedee0d85b26
Abodawoud <yousefabodawoud@gmail.com>
2023-06-10 00:01:39 +0300

    I guess that 5 is correct

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    list1 = []
+    list1[:0] = my_string
+    for str in list1:
+        if str == 'c' or str == 'C':
+            index = list1.index(str)
+            del list1[index]
+    new_string = ''.join(list1)
+    return (new_string)
