f1f6936750aeb068298974939d0a8046df0f543f
Abodawoud <yousefabodawoud@gmail.com>
2023-06-10 11:49:54 +0300

    task 5 again

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     list1 = []
-    list1[:0] = my_string
+    list1[:] = my_string
     for str in list1:
         if str == 'c' or str == 'C':
             index = list1.index(str)
