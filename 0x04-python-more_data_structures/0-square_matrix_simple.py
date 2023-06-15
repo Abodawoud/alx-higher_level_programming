9fe35c62b745cbee233b435a0a6087a87d76029e
Abodawoud <yousefabodawoud@gmail.com>
2023-06-15 06:23:40 +0300

    task 0 again

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,9 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    new_list = []
     for rows in range(len(matrix)):
-        for v in range(3):
+        new_list = []
+        for v in range(len(matrix[rows])):
             new_list.append(matrix[rows][v] ** 2)
         new_matrix.append(new_list)
-        new_list = []
     return (new_matrix)
