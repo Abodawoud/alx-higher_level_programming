28d25b5644660c002782d7620e420662fa901783
Abodawoud <yousefabodawoud@gmail.com>
2023-06-26 19:41:56 +0300

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except Exception as e:
+        except IndexError:
             break
         else:
             num_of_nums += 1
