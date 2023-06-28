ef3c756a29e3d974d6bb3e6da66a15418da845d1
Abodawoud <yousefabodawoud@gmail.com>
2023-06-29 01:37:40 +0300

    task - 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+
+"""Square Task"""
+
+
+class Square:
+    """ Empty """
+    def __init__(self, size=0):
+        """Exceptions are documented in the same way as classes.
+
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+
+    Attributes:
+        size (int): Size of the Square.
+    """
+        if isinstance(size, int) != True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
