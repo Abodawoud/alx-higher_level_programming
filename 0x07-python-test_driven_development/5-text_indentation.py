d8299221e005d5ec41f3a95b291d153a1f54e517
Abodawoud <yousefabodawoud@gmail.com>
2023-07-06 04:53:48 +0300

    task5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""text_indentation module"""
+
+
+def text_indentation(text):
+    """text_indentation"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = text[:]
+    for special_character in ".?:":
+        new_list = new_text.split(special_character)
+        new_text = ""
+        for i in new_list:
+            i = i.strip(" ")
+            if new_text == "":
+                new_text = i + special_character
+            else:
+                new_text =  new_text + "\n\n" + i + special_character
+    print(new_text[:-3], end="")
