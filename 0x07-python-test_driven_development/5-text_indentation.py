d5f8bd4baadba0dbc27db8d913be47cfce4c8968
Abodawoud <yousefabodawoud@gmail.com>
2023-07-06 04:54:36 +0300

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -17,5 +17,5 @@ def text_indentation(text):
             if new_text == "":
                 new_text = i + special_character
             else:
-                new_text =  new_text + "\n\n" + i + special_character
+                new_text = new_text + "\n\n" + i + special_character
     print(new_text[:-3], end="")
