a14dc6c34f8b18e36674cefd7cb8ea3d979f1dda
Abodawoud <yousefabodawoud@gmail.com>
2023-07-18 00:44:52 +0300

    base 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,6 +43,6 @@ class Base:
         """function that writes an Object to a text file,
         using a JSON representation"""
 
-        if json_string is None or json_string is "":
+        if json_string is None or json_string == "":
             return "[]"
         return json.loads(json_string)
