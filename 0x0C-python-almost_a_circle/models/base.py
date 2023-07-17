9caa60d3c8b6a2d26956c225840f93c8c05f4fb1
Abodawoud <yousefabodawoud@gmail.com>
2023-07-17 23:57:54 +0300

    base 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,18 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """function that writes an Object to a text file,
+        using a JSON representation"""
+        len_list = len(list_objs)
+        filename = cls.__name__ + ".json"
+        print(len_list)
+        new_list = []
+        for i in range(0, len_list):
+            json_dict = cls.to_dictionary(list_objs[i])
+            new_list.append(json_dict)
+            js_str = cls.to_json_string(new_list)
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(js_str)
