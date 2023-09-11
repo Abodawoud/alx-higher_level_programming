82a6c02793c3b8fa61779341432eba04c9ad82d8
Abodawoud <yousefabodawoud@gmail.com>
2023-09-11 23:37:24 +0300

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,13 @@ const { argv } = require('process');
 const log = console.log;
 let num = Number(argv[2]);
 
-if (isNaN(num) || argv.length === 2 || num < 0) {
+if (isNaN(num) || argv.length === 2) {
   log('Missing number of occurrences');
 } else {
   while (num !== 0) {
+    if (num < 0) {
+      break;
+    }
     log('C is fun');
     num--;
   }
