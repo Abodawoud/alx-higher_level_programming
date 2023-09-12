1ab1b58931aeea8a7688f363a3561764fdfdf94f
Abodawoud <yousefabodawoud@gmail.com>
2023-09-13 02:12:22 +0300

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let newArr = [];
-  let i = 0
-  let len = list.length - 1
+  const newArr = [];
+  let i = 0;
+  let len = list.length - 1;
   while (len) {
     newArr[i] = list[len];
     len--;
     i++;
   }
-  return newArr
+  return newArr;
 };
