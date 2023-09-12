c6ba3a70675dfb33a58cae41c782324a6d1b3769
Abodawoud <yousefabodawoud@gmail.com>
2023-09-13 02:00:37 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,18 +2,13 @@
 const PreSquare = require('./5-square.js');
 
 class Square extends PreSquare {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
   charPrint (c) {
     if (c === undefined) {
       super.print();
     } else {
-      let rows = this.size;
+      let rows = this.width;
       while (rows) {
-        console.log(c.repeat(this.size));
+        console.log(c.repeat(this.width));
         rows--;
       }
     }
