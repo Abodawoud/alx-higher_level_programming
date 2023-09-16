2840124806995025a429f11c24fcd7c1e4b5699d
Abodawoud <yousefabodawoud@gmail.com>
2023-09-16 06:55:05 +0300

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Use MySQLdb module"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_user = argv[1]
+    mysql_passwd = argv[2]
+    mysql_db = argv[3]
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
