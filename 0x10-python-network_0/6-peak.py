e25ab1de7605aa370d3053924c09353c8bab8349
Abodawoud <yousefabodawoud@gmail.com>
2023-09-29 23:03:47 +0300

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def find_peak(lst):
+    if not lst:
+        return None
+    elif len(lst) == 1:
+        return lst[0]
+    elif len(lst) == 2:
+        return max(lst)
+    for i in range(1, len(lst) - 1):
+        if lst[i] > lst[i - 1] and lst[i] > lst[i + 1]:
+            return lst[i]
+
+    return max(lst[0], lst[-1])
