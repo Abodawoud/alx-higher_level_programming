749f189f4edb52424ebf1c8915219bbb056e511d
Abodawoud <yousefabodawoud@gmail.com>
2023-09-30 21:46:08 +0300

    task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,11 +6,16 @@ import sys
 
 if __name__ == "__main__":
     headers = {
-        "Accept": "application/vnd.github+json",
+        "Accept": "application/vnd.github.v3+json",
         "Authorization": f"Bearer {sys.argv[2]}",
         "X-GitHub-Api-Version": "2022-11-28",
     }
     url = f'https://api.github.com/user'
 
     response = requests.get(url, headers=headers)
-    print(response.text)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data["id"])
+    else:
+        print("None")
