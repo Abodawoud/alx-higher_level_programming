678023b36add1e0bd05cbca00906e3197ed33e2f
Abodawoud <yousefabodawoud@gmail.com>
2023-10-01 01:00:35 +0300

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that fetches github"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    url = f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits'
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        for i in range(0, 10):
+            print(f'{user_data[i].get("sha")}: {user_data[0].get("commit").get("author").get("name")}')
+    else:
+        print("None")
