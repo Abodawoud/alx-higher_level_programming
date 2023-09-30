4f09cf6a5a945cf071b3d72de521edadb7c34dc3
Abodawoud <yousefabodawoud@gmail.com>
2023-10-01 01:56:09 +0300

    task 100-2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     response = requests.get(url)
 
     user_data = response.json()
-    i = 9
+    i = -1
     for _ in range(10):
         print(f'{user_data[i].get("sha")}: \
 {user_data[i].get("commit").get("author").get("name")}')
