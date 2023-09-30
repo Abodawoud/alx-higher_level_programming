b4ba903b10b11ef98cdd4969cc7245fb79a1647d
Abodawoud <yousefabodawoud@gmail.com>
2023-09-30 21:02:47 +0300

    task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,15 +4,16 @@ import requests
 import sys
 
 
-headers = {
-    'Authorization': sys.argv[2],
-    'Accept': 'application/vnd.github.v3+json',
-}
-url = f'https://api.github.com/users/{sys.argv[1]}'
+if __name__ == "__main__":
+    headers = {
+        'Authorization': sys.argv[2],
+        'Accept': 'application/vnd.github.v3+json',
+    }
+    url = f'https://api.github.com/user'
 
 
-response = requests.get(url, headers=headers)
-if response.status_code >= 400:
-    print("None")
-else:
-    print(response.json()["id"])
+    response = requests.get(url, headers=headers)
+    if response.status_code >= 400:
+        print("None")
+    else:
+        print(response.json()["id"])
