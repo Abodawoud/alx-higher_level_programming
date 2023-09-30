089b2f018f1513c929fb7eb72c5aaa800ea0110d
Abodawoud <yousefabodawoud@gmail.com>
2023-09-30 21:32:43 +0300

    task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Python script that fetches github"""
-import requests
+"""
+Python script that takes GitHub credentials
+and uses the GitHub API to display the user id
+"""
+from requests import get, auth
 import sys
 
 
 if __name__ == "__main__":
-    headers = {
-        'Authorization': f'Bearer {sys.argv[2]}',
-        'X-GitHub-Api-Version': '2022-11-28',
-        'Accept': 'application/vnd.github+json'
-    }
-    url = f'https://api.github.com/users/{sys.argv[1]}'
-
-    response = requests.get(url, headers=headers)
-    print(response)
-    print(response.json().get('id'))
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
