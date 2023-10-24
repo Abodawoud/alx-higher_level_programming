2a00dd96857f382de2aa7fdc6ae4d8e5aa7c1e5f
Abodawoud <yousefabodawoud@gmail.com>
2023-10-24 19:17:57 +0300

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) { console.error(err); } else { console.log(data); }
+  if (err) { console.log(err); } else { console.log(data); }
 });
