aa12ad468806b30fd3b720df262dcf19099e9f6b
Abodawoud <yousefabodawoud@gmail.com>
2023-10-24 21:22:56 +0300

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,9 +5,6 @@ request.get(`${process.argv[2]}?completed=true`, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
-  if (res.statusCode !== 200) {
-    return console.error(`Failed to fetch data from the API. Status code: ${res.statusCode}`);
-  }
 
   const todos = JSON.parse(body);
   const completedTaskCount = {};
